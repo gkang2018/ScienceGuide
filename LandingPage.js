2311dfa51d82a1f303fcd50b343fff6e07f3d645
heartface99 <echoi21@bu.edu>
2020-02-29 17:01:32 -0500

    skeleton of landing page done

--- LandingPage.js
+++ LandingPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, Button, View, Alert } from 'react-native';
 
 export default function LandingPage() {
   return (
     <View style={styles.container}>
-      <Text>Science Guide</Text>
+      <Text style={styles.title}>Science Guide</Text>
+      <Image style={{width: 250, height: 250, marginBottom: 100}} source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}} />
+     <View style={styles.smallTextContainer}>
+      <Button
+        title="Let's get started!"
+        onPress={() => Alert.alert('Button pressed')}
+      />
+      <Text style={styles.smallText}>Already have an account?</Text>
+	<Text>Sign In</Text>
+     </View>
     </View>
   );
 }
@@ -16,4 +25,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  smallTextContainer: {
+   marginBottom: 60,
+   alignItems: 'center',
+  },
+  smallText: {
+    marginTop: 20,
+  },
+  title: {
+    flex: 1,
+    marginTop: 60,
+    fontWeight: 'bold',
+    fontSize: 30,
+    color: '#a9a9a9'
+  },
+  
 });
