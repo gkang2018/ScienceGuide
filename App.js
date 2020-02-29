b430f1d622a183b96d1f0611cb40739a0eb6a5c1
heartface99 <echoi21@bu.edu>
2020-02-29 17:10:52 -0500

    organization

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import LandingPage from './LandingPage.js';
+import LandingPage from './components/LandingPage.js';
 
 export default function App() {
   return (
