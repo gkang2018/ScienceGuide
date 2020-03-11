40795fa049f99a6271f20e6bdfda90fccbce0ed7
Gagandeep Kang <gskang@bu.edu>
2020-03-11 10:38:32 -0400

    handle adding

--- components/InterestsCard.js
+++ components/InterestsCard.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, useState } from "react-native";
 
 export default function InterestsCard(props, { navigation }) {
+  const selectedLevel = props.interest;
   return (
     <View>
-      <Card style={styles.container}>
+      <Card>
         <Card.Cover source={{ uri: props.image }} />
+        <Card.Title title={selectedLevel} />
         <Card.Actions>
           <Button>Cancel</Button>
-          <Button>Ok</Button>
+          <Button onPress={props.onAddSelected.bind(this, selectedLevel)}>
+            Ok
+          </Button>
         </Card.Actions>
       </Card>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center"
-  }
-});
--- components/ResearchInterests.js
+++ components/ResearchInterests.js
@@ -5,25 +5,33 @@ import InterestsCard from "./InterestsCard";
 export default function ResearchInterests() {
   const [selectedLevels, setSelectedLevels] = useState([]);
 
-  const handleAddLevel = () => {
-    setSelectedLevels(oldArray => [...oldArray, newElement]);
+  const handleAddLevel = selectedLevel => {
+    setSelectedLevels(oldArray => [
+      ...oldArray,
+      { id: Math.random().toString(), value: selectedLevel }
+    ]);
+    console.log(selectedLevels);
   };
 
   const interestsData = [
-    { image: "../assets/icon.png" },
-    { image: "../assets/icon.png" },
-    { image: "../assets/icon.png" },
-    { image: "../assets/icon.png" }
+    { interest: "math", image: "../assets/icon.png" },
+    { interest: "science", image: "../assets/icon.png" },
+    { interest: "physics", image: "../assets/icon.png" },
+    { interest: "biology", image: "../assets/icon.png" }
   ];
 
   return (
     <View>
       <Text h1>Research Interests</Text>
-      <Text h4></Text>
+      <Text h4> </Text>
       <FlatList
         data={interestsData}
         renderItem={({ item }) => (
-          <InterestsCard onPress={handleAddLevel} image={item.image} />
+          <InterestsCard
+            interest={item.interest}
+            onAddSelected={handleAddLevel}
+            image={item.image}
+          />
         )}
         numColumns={2}
       />
