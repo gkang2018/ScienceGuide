49a9e76435da7d73fb80931be2c560217284a158
Gagandeep Kang <gskang@bu.edu>
2020-03-17 14:25:58 -0400

    added firebase config

---
+++ config/firebase.js
@@ -0,0 +1,32 @@
+import firebase from "firebase/app";
+
+import "firebase/firestore";
+
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID
+} from "react-native-dotenv";
+
+var firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const firestore = firebase.firestore();
+
+export default firebase;
