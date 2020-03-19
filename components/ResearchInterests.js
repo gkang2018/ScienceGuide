88dc310af186368ab30591bae86a34e933dc58a3
Gagandeep Kang <gskang@bu.edu>
2020-03-19 14:18:13 -0400

    added interest data file

--- components/ResearchInterests.js
+++ components/ResearchInterests.js
@@ -1,64 +1,34 @@
-import React, { useState, useLayoutEffect, Component } from "react";
+import React, { useState, Component, useLayoutEffect } from "react";
 import { View, Text, FlatList, StyleSheet, Button } from "react-native";
 import { Title, Subheading } from "react-native-paper";
 import InterestsCard from "./InterestsCard";
+import { interestsData } from "../interestData";
 
 import { connect } from "react-redux";
 
 class ResearchInterests extends Component {
-  state = {
-    subText: "Select up to three research areas you are interested in"
+  constructor(props) {
+    super(props);
+  }
+
+  static navigationOptions = function(props) {
+    return {
+      headerRight: () => (
+        <Button
+          title="Hello"
+          onPress={this.props.navigation.navigate("Areas")}
+        ></Button>
+      )
+    };
   };
 
-  // fix rendering the subtext
-
-  componentDidMount() {
-    this.changeSubText();
-  }
-  componentDidUpdate(prevProps) {
-    if (this.props.interests.length != prevProps.interests.length) {
-      this.changeSubText();
-    }
-  }
-
-  changeSubText() {
-    switch (this.props.interests.length) {
-      case 1:
-        this.setState({
-          subText: "Select two more research areas you are interested in"
-        });
-      case 2:
-        this.setState({
-          subText: "Select one or more research area you are interested in"
-        });
-      case 3:
-        this.setState({
-          subText: "You may review your selections in the next screen"
-        });
-      default:
-        this.setState({
-          subText: "Select up to three research areas you are interested in"
-        });
-    }
-  }
-
   render() {
-    const interestsData = [
-      { id: 0, interest: "math", image: "../assets/icon.png" },
-      { id: 1, interest: "science", image: "../assets/icon.png" },
-      { id: 2, interest: "physics", image: "../assets/icon.png" },
-      { id: 3, interest: "biology", image: "../assets/icon.png" }
-    ];
     return (
       <View>
         <View style={styles.title}>
           <Title>Research Interests</Title>
         </View>
         <Subheading>{this.subText}</Subheading>
-        <Button
-          title="Next"
-          onPress={() => this.props.navigation.navigate("Areas")}
-        />
         <FlatList
           style={{ width: "100%" }}
           data={interestsData}
---
+++ interestData.js
@@ -0,0 +1,6 @@
+export const interestsData = [
+  { id: 0, interest: "math", image: "../assets/icon.png" },
+  { id: 1, interest: "science", image: "../assets/icon.png" },
+  { id: 2, interest: "physics", image: "../assets/icon.png" },
+  { id: 3, interest: "biology", image: "../assets/icon.png" }
+];
