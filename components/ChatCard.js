701e9a8150489be7c6a5fd1dc15e12437cc3bc4c
Gagandeep Kang <gskang@bu.edu>
2020-04-02 18:15:30 -0400

    created chat card

---
+++ components/ChatCard.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+
+class ChatCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <TouchableOpacity>
+        <View style={styles.container}>
+          <Image
+            style={styles.avatar}
+            source={{ uri: this.props.profileImage }}
+          />
+          <View style={styles.details}>
+            <Text style={styles.recipient}>{this.props.recipient}</Text>
+            <Text style={styles.lastMessage}>{this.props.lastMessage}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  avatar: {
+    height: 100,
+    width: 100,
+    borderRadius: 100 / 2,
+    margin: 10
+  },
+  details: {
+    marginLeft: 35,
+    paddingTop: 20,
+    marginRight: 125
+  },
+  container: {
+    borderWidth: 2,
+    flexDirection: "row",
+    marginLeft: 25,
+    marginRight: 25,
+    marginBottom: 15
+  }
+});
+
+export default ChatCard;
--- components/MessagesScreen.js
+++ components/MessagesScreen.js
@@ -1,17 +1,44 @@
 import React, { Component } from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import ChatCard from "./ChatCard";
 
 class MessagesScreen extends Component {
   constructor(props) {
     super(props);
+
+    // pull the chatrooms that the user currently has
+    this.state = {
+      userChatRooms: ["", "", "", ""]
+    };
   }
 
   render() {
+    if (this.state.userChatRooms == undefined) {
+      return (
+        <View>
+          <View style={styles.heading}>
+            <Text style={styles.title}>Messages</Text>
+          </View>
+          <View style={styles.container}>
+            <ActivityIndicator size="large" color="#0000ff" animating={true} />
+          </View>
+        </View>
+      );
+    }
     return (
       <View>
         <View style={styles.heading}>
           <Text style={styles.title}>Messages</Text>
+        </View>
+        <View>
+          <FlatList
+            data={this.state.userChatRooms}
+            renderItem={({ item }) => (
+              <ChatCard recipient={"Gagan"} lastMessage={"Hello World "} />
+            )}
+          />
         </View>
       </View>
     );
@@ -28,6 +55,12 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 25,
     fontWeight: "700"
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "column",
+    marginTop: 50
   }
 });
 
