3219d62cad852fdf2f48d66b5f310418636eabf3
Norman A. Toro Vega <normant@bu.edu>
2020-04-06 17:35:04 -0400

    Front End on Screen 1 is working

--- components/ResearchLevel.js
+++ components/ResearchLevel.js
@@ -6,66 +6,90 @@ import { addLevel } from "../actions/actions";
 class ResearchLevel extends Component {
   render() {
     return (
-      <View>
-        <View style={styles.title}>
+      <View style= {styles.mainContainer}>
+
+
+        <View style={styles.titleContainer}>
           <Text style={styles.titleText}>Research Level</Text>
           <Text style={styles.descriptionText}>
             Please select what level of research you have previously done.
           </Text>
         </View>
+        
+
         <View style={styles.form}>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.addLevel("Beginner");
-              this.props.navigation.navigate("Interests");
-            }}
-          >
-            <Text style={styles.formText}>
-              I want to make my first research project!
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.addLevel("Intermediate");
-              this.props.navigation.navigate("Interests");
-            }}
-          >
-            <Text style={styles.formText}>
-              I won at my school but want to win nationally!
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              this.props.addLevel("Experienced");
-              this.props.navigation.navigate("Interests");
-            }}
-          >
-            <Text style={styles.formText}>
-              I won nationally and want to further the impact of my research!
-            </Text>
-          </TouchableOpacity>
+          <View style = {styles.researchLevelContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.addLevel("Beginner");
+                this.props.navigation.navigate("Interests");
+              }}
+            >
+              <Text style={styles.formText}>
+                I want to make my first research project!
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+
+          <View style = {styles.researchLevelContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.addLevel("Intermediate");
+                this.props.navigation.navigate("Interests");
+              }}
+            >
+              <Text style={styles.formText}>
+                I won at my school but want to win nationally!
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+
+          <View style = {styles.researchLevelContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                this.props.addLevel("Experienced");
+                this.props.navigation.navigate("Interests");
+              }}
+            >
+              <Text style={styles.formText}>
+                I won nationally and want to further the impact of my research!
+              </Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
+
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  title: {
-    paddingLeft: 50,
-    textAlign: "center"
+  mainContainer: {
+    flex: 1
+  },
+  titleContainer: {
+  },
+  researchLevelContainer: {
+    alignItems: "center",
+    marginBottom: 20
   },
   titleText: {
     fontSize: 40,
     marginTop: 100,
-    fontWeight: "700"
+    fontWeight: "700",
+    textAlign: 'center'
   },
   descriptionText: {
-    fontSize: 20,
-    marginTop: 20
+    fontSize: 17,
+    marginTop: 20,
+    textAlign: 'center',
   },
   form: {
-    paddingLeft: 95,
+    //paddingLeft: 95,
     marginTop: 100
   },
   formText: {
@@ -78,8 +102,8 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     fontSize: 15,
     textAlign: "center",
-    marginRight: 90,
-    marginBottom: 10
+    //marginRight: 90,
+    //marginBottom: 10
   }
 });
 
