e542f19a531ccb29763419f9288e18b64c8b60ab
Norman A. Toro Vega <normant@bu.edu>
2020-04-25 19:34:07 -0400

    Mentor Match Screen mostly done

--- screens/ResearchAreas.js
+++ screens/ResearchAreas.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
 
 import { connect } from "react-redux";
 import * as RNLocalize from 'react-native-localize'
@@ -74,30 +74,38 @@ class ResearchAreas extends Component {
           </View>
 
 
+
+
           <View style={styles.areasContainer}>
-            <View style={styles.firstArea}>
-              <Text style={styles.firstInterest}>
-                {this.state.firstInterest}
-              </Text>
-            </View>
-
-            <View style={styles.secondArea}>
-              <Text style={styles.secondInterest}>
-                {this.state.secondInterest}
-              </Text>
-            </View>
-
-            <View style={styles.thirdArea}>
-              <Text style={styles.thirdInterest}>
-                {this.state.thirdInterest}
-              </Text>
-            </View>
-
-            <TouchableOpacity onPress={() => this.props.navigation.navigate("AvailableMentors")}>
-              <View style={styles.matchButton}>
-                <Text style={styles.matchText}>{this.localize.translate("researchAreas.findMentor")}</Text>
+            <ImageBackground style={styles.image} source={require('../assets/black.png')}>
+              <View style={styles.firstArea}>
+                <Text style={styles.firstInterest}>
+                  {this.state.firstInterest}
+                </Text>
               </View>
-            </TouchableOpacity>
+
+              <View style={styles.secondArea}>
+                <Text style={styles.secondInterest}>
+                  {this.state.secondInterest}
+                </Text>
+              </View>
+
+              <View style={styles.thirdArea}>
+                <Text style={styles.thirdInterest}>
+                  {this.state.thirdInterest}
+                </Text>
+              </View>
+
+              <TouchableOpacity onPress={() => this.props.navigation.navigate("AvailableMentors")}>
+                <View style={styles.matchButton}>
+                  <Text style={styles.matchText}>{this.localize.translate("researchAreas.findMentor")}</Text>
+                </View>
+              </TouchableOpacity>
+            </ImageBackground>
+
+
+
+
           </View>
 
 
@@ -117,7 +125,7 @@ const mapStateToProps = state => {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   heading: {
     flex: 1,
@@ -129,77 +137,99 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: RFPercentage(5),
-    //    fontSize: 40,
     marginTop: '10%',
     fontWeight: "700",
     textAlign: 'center',
   },
   areasContainer: {
     flex: 3,
-    //flexDirection: "row",
-    //backgroundColor: 'yellow',
+    flexDirection: "row",
+    //backgroundColor: 'red',
     justifyContent: 'center',
-    alignItems: 'center'
+    
+    //alignItems: 'center',
   },
 
   firstArea: {
-    width: 115,
-    height: 115,
-    borderWidth: 1,
-    borderRadius: 115 / 2,
     position: 'absolute',
-    top: 20,
-    left: 20,
+    top: '10%',
+    left: '10%',
+    width: 100,
+    height: 100,
+    borderWidth: 8,
+    borderRadius: 115 / 2,
+    borderColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
 
   },
   secondArea: {
-    width: 115,
-    height: 115,
-    borderWidth: 1,
-    borderRadius: 115 / 2,
     position: 'absolute',
-    top: 300,
-    left: 150,
+    top: '70%',
+    //left: '38%',
+    width: 100,
+    height: 100,
+    borderWidth: 8,
+    borderRadius: 115 / 2,
+    borderColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
   thirdArea: {
-    width: 115,
-    height: 115,
-    borderWidth: 1,
-    borderRadius: 115 / 2,
     position: 'absolute',
-    top: 20,
-    left: 300,
+    top: '10%',
+    right: '10%',
+    width: 100,
+    height: 100,
+    borderWidth: 8,
+    borderRadius: 115 / 2,
+    borderColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
   },
-  firstInterest: {
-    paddingLeft: 20,
-    paddingTop: 50
-  },
-  secondInterest: {
-    paddingLeft: 20,
-    paddingTop: 50
-  },
-  thirdInterest: {
-    paddingLeft: 20,
-    paddingTop: 50
-  },
-
-
   matchButton: {
+    //position: 'absolute',
+    top: '35%',
+    //left: '35%',
     width: 125,
     height: 125,
-    borderWidth: 1,
+    borderWidth: 8,
     borderRadius: 125 / 2,
+    borderColor: 'yellow',
     //marginLeft: 115,
-    marginBottom: 50,
-    backgroundColor: 'green',
-    //position: 'absolute',
+    //marginBottom: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  firstInterest: {
+    fontSize: RFPercentage(1.4),
+    textAlign: 'center'
+  },
+  secondInterest: {
+    fontSize: RFPercentage(1.4),
+    textAlign: 'center'
+  },
+  thirdInterest: {
+    fontSize: RFPercentage(1.4),
+    textAlign: 'center'
   },
   matchText: {
-    paddingLeft: 20,
-    paddingTop: 50,
-    fontSize: 15,
-    fontWeight: "700"
-  }
+    fontSize: RFValue(15),
+    fontWeight: '700',
+    textAlign: 'center'
+  },
+  image: {
+    flex: 1,
+    //resizeMode: "cover",
+    flexDirection: "row",
+    overflow: 'visible',
+    //backgroundColor: 'red',
+    justifyContent: 'center',
+  },
 });
 
 export default connect(mapStateToProps, null)(ResearchAreas);
